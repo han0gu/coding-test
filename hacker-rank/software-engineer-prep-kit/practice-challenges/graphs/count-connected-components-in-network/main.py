764c53ca5142a83ec2a36857783617641314a196
han0gu <imgum9@gmail.com>
2026-05-25 00:55:44 +0900

    docs(count-connected-components-in-network): link가 하나도 없는 독립 node도 정답에 포함시키도록 개선

--- hacker-rank/software-engineer-prep-kit/practice-challenges/graphs/count-connected-components-in-network/main.py
+++ hacker-rank/software-engineer-prep-kit/practice-challenges/graphs/count-connected-components-in-network/main.py
@@ -27,51 +27,53 @@
 1 1 0
 """
 
+"""
+1
+2
+0 1
+3
+
+0 1 0
+1 0 0
+0 0 0
+"""
+
 def paint_with_one(board, start, end):
     board[start][end] = 1
     board[end][start] = 1
-    
-def add_near(board, row, col, stack):
-    if 0 <= row < n and 0 <= col < n and board[row][col] == 1:
-        stack.append((row, col))
-    
+
 def countIsolatedCommunicationGroups(links, n):
-    
-    if not links:
-        return 0
-        
+
     if n == 1:
         return 1
-        
+
     answer = 0
-    
+
     board = [[0] * n for _ in range(n)]
     # print('init board', board)
-    
+
     for start, end in links:
         paint_with_one(board, start, end)
     # print('paint board', board)
-    
+
+    visited = [False] * n
     stack = []
-    for row in range(n):
-        for col in range(n):
-            if board[row][col] == 1:
-                answer += 1
-                stack.append((row, col))
-                # print('init stack', stack)
-                
-                while stack:
-                    row, col = stack.pop()
-                    board[row][col] = 0
-                    
-                    add_near(board, row, col+1, stack)
-                    add_near(board, row+1, col-1, stack)
-                    add_near(board, row+1, col, stack)
-                    add_near(board, row+1, col+1, stack)
-                    # print('stack', stack)
-                    
-            # print('board', board)
-                
+    for node in range(n):
+        if visited[node]:
+            continue
+
+        answer += 1
+        visited[node] = True
+        stack.append(node)
+
+        while stack:
+            cur = stack.pop()
+
+            for near in range(n):
+                if not visited[near] and board[cur][near] == 1:
+                    visited[near] = True
+                    stack.append(near)
+
     return answer
 
 if __name__ == '__main__':
