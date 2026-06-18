b56c10ef545a9ebe5e7785464a1bb060f3afa079
han0gu <imgum9@gmail.com>
2026-06-18 18:34:51 +0900

    docs(1182): Gemini feedback을 반영하여 입력 처리 개선

--- 문제집/백트래킹/1182-부분수열의-합/main.py
+++ 문제집/백트래킹/1182-부분수열의-합/main.py
@@ -1,19 +1,28 @@
-n, target = map(int, input().split())
-nums = list(map(int, input().split()))
+import sys
 
-answer = 0
 
-stack = [(0, 0, 0)] # (index, current_sum, selected_count)
+def main():
+    input = sys.stdin.readline
 
-while stack:
-    idx, value, count = stack.pop()
+    n, target = map(int, input().split())
+    nums = list(map(int, input().split()))
 
-    if idx == n:
-        if value == target and count > 0:
-            answer += 1
-        continue
+    answer = 0
+    stack = [(0, 0, 0)]  # (index, current_sum, selected_count)
 
-    stack.append((idx + 1, value + nums[idx], count + 1))
-    stack.append((idx + 1, value, count))
+    while stack:
+        idx, value, count = stack.pop()
 
-print(answer)
+        if idx == n:
+            if value == target and count > 0:
+                answer += 1
+            continue
+
+        stack.append((idx + 1, value + nums[idx], count + 1))
+        stack.append((idx + 1, value, count))
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
