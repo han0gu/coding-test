5cda7d0a76a724647a10c3d50098181b177eb5ea
han0gu <imgum9@gmail.com>
2026-06-19 20:27:11 +0900

    docs(programmers): 여행 경로 / DFS로 풀이

--- programmers/DFS-BFS/여행-경로/main.py
+++ programmers/DFS-BFS/여행-경로/main.py
@@ -1,18 +1,16 @@
-from collections import deque
-
 def solution(tickets):
     answer = []
-    n = len(tickets)
-
-    q = deque([('ICN', 0, [False] * n, ['ICN'])]) # (도착지, 사용한 티켓 개수, 티켓 사용 여부 리스트, answer)
     
-    while q:
-        cur_e, used_cnt, visited, cur_answer = q.popleft()
+    n = len(tickets)
+    stack = [('ICN', 0, [False] * n, ['ICN'])] # (도착지, 사용한 티켓 수, 티켓 사용 여부, 정답 리스트)
+    
+    while stack:
+        cur_e, cnt, visited, cur_answer = stack.pop()
         
-        if used_cnt == n:
+        if cnt == n:
             answer.append(cur_answer)
             continue
-        
+            
         for i, (nxt_s, nxt_e) in enumerate(tickets):
             if not visited[i] and cur_e == nxt_s:
                 new_visited = [*visited]
@@ -21,6 +19,7 @@ def solution(tickets):
                 new_visited[i] = True
                 new_answer.append(nxt_e)
                 
-                q.append( (nxt_e, used_cnt + 1, new_visited, new_answer) )
+                stack.append( (nxt_e, cnt + 1, new_visited, new_answer) )
     
     return min(answer)
+    
