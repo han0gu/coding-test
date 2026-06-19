928496aa6a3d8cd6e99254138e54940d3c20405f
han0gu <imgum9@gmail.com>
2026-06-19 13:27:58 +0900

    docs(14501): Codex feedback을 추가 반영하여 백트래킹 로직 개선

--- 문제집/백트래킹/14501-퇴사/main.py
+++ 문제집/백트래킹/14501-퇴사/main.py
@@ -2,15 +2,7 @@ n = int(input())
 candidates = [tuple(map(int, input().split())) for _ in range(n)] # need, profit
 
 answer = 0
-stack = [] # nxt_idx, cur_acc
-
-# 첫 업무
-for idx, (need, profit) in enumerate(candidates):
-    if idx + need - 1 < n:
-        stack.append((idx + 1, 0)) # 미선택
-        stack.append((idx + need, profit)) # 선택
-        break
-# print('init stack', stack)
+stack = [(0,0)] # nxt_idx, cur_acc
 
 while stack:
     nxt_idx, cur_acc = stack.pop()
@@ -21,7 +13,7 @@ while stack:
 
     nxt_need, nxt_profit = candidates[nxt_idx]
 
-    if nxt_idx + nxt_need - 1 < n:
+    if nxt_idx + nxt_need <= n:
         stack.append((nxt_idx + 1, cur_acc)) # 미선택
         stack.append((nxt_idx + nxt_need, cur_acc + nxt_profit)) # 선택
     else:
