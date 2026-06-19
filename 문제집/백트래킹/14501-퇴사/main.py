233faa47ad10ab6aa04d73b655fc7f57a85d9948
han0gu <imgum9@gmail.com>
2026-06-19 13:31:56 +0900

    docs(14501): Codex feedback 문서를 최신화

--- 문제집/백트래킹/14501-퇴사/main.py
+++ 문제집/백트래킹/14501-퇴사/main.py
@@ -1,8 +1,8 @@
 n = int(input())
-candidates = [tuple(map(int, input().split())) for _ in range(n)] # need, profit
+candidates = [tuple(map(int, input().split())) for _ in range(n)]  # need, profit
 
 answer = 0
-stack = [(0,0)] # nxt_idx, cur_acc
+stack = [(0, 0)]  # nxt_idx, cur_acc
 
 while stack:
     nxt_idx, cur_acc = stack.pop()
@@ -13,11 +13,9 @@ while stack:
 
     nxt_need, nxt_profit = candidates[nxt_idx]
 
+    stack.append((nxt_idx + 1, cur_acc))  # 미선택
+
     if nxt_idx + nxt_need <= n:
-        stack.append((nxt_idx + 1, cur_acc)) # 미선택
-        stack.append((nxt_idx + nxt_need, cur_acc + nxt_profit)) # 선택
-    else:
-        stack.append((nxt_idx + 1, cur_acc)) # 미선택
-    # print('stack', stack)
+        stack.append((nxt_idx + nxt_need, cur_acc + nxt_profit))  # 선택
 
 print(answer)
