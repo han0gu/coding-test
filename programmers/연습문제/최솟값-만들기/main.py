5aaee6cf57f63bae889905b1c70c3064ab7658fe
han0gu <imgum9@gmail.com>
2026-06-20 10:53:44 +0900

    docs(programmers): 최솟값 만들기 / 아이디어로 풀이

--- programmers/연습문제/최솟값-만들기/main.py
+++ programmers/연습문제/최솟값-만들기/main.py
@@ -1,17 +1,10 @@
-from itertools import permutations
-
 def solution(a, b):
-    
     answer = 0
-    n = len(a)
     
-    for p in permutations(b, n):
-        tmp = 0
-        
-        for i in range(n):
-            tmp += a[i] * p[i]
-            # print('tmp', tmp)
+    a.sort()
+    b.sort(reverse=True)
     
-        answer = min(answer, tmp) if answer != 0 else tmp
+    for i in range(len(a)):
+        answer += a[i] * b[i]
         
     return answer
