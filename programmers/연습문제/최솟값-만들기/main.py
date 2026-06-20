0296031b9925b533e9b1cf45bee04fe456b2382e
han0gu <imgum9@gmail.com>
2026-06-20 11:22:26 +0900

    docs(programmers): 최솟값 만들기 / Gemini feedback 반영하여 풀이 개선

--- programmers/연습문제/최솟값-만들기/main.py
+++ programmers/연습문제/최솟값-만들기/main.py
@@ -1,10 +1,5 @@
 def solution(a, b):
-    answer = 0
-
     a.sort()
     b.sort(reverse=True)
-
-    for i in range(len(a)):
-        answer += a[i] * b[i]
-
-    return answer
+    
+    return sum(x * y for x, y in zip(a, b))
